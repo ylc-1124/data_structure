548aee2ded1421484436fc9c8e9195578227f377
ylc-1124 <853703920@qq.com>
2021-08-12 10:42:41 +0800

    消除警告

--- 07-RedBlackTree/src/ylc/Test.java
+++ 07-RedBlackTree/src/ylc/Test.java
@@ -2,7 +2,7 @@ package ylc;
 
 
 import ylc.printer.BinaryTrees;
-import ylc.tree.AVLTree;
+
 import ylc.tree.RBTree;
 
 public class Test {
@@ -10,13 +10,13 @@ public class Test {
         test1();
     }
     static void test1() {
-        Integer data[] = new Integer[] {
+        Integer[] data = new Integer[] {
                 78, 31, 27, 18, 77, 12, 46, 17, 11, 2, 97, 52
         };
 
         RBTree<Integer> rbTree = new RBTree<>();
-        for (int i = 0; i < data.length; i++) {
-            rbTree.add(data[i]);
+        for (Integer datum : data) {
+            rbTree.add(datum);
 
         }
         BinaryTrees.println(rbTree);
